ea602cf5fbbfa2545044d1f1482335bd5b293223
Robby Lenovo <Robert@gmail.com>
2021-08-22 17:15:50 +0200

    werkdag bij klaas en likki

--- Board.py
+++ Board.py
@@ -1,36 +1,50 @@
 import numpy as np
-from numpy.lib.shape_base import column_stack
-
-class Board:
-    def __init__(self, assignment):
-        self.assignment = assignment
+from numpy.core.numeric import isfortran
+from numpy.matrixlib.defmatrix import matrix
 
 
 ex1 = np.array(
-[[3, 4, np.nan , 2, 9, 8, 7, np.nan, 1],
-[9,6,np.nan, np.nan, 5, np.nan, np.nan, np.nan, np.nan],
-[8,7, np.nan,6, np.nan,1,5,9,3],
-[1, np.nan, np.nan, np.nan, 2, np.nan, np.nan, np.nan, np.nan],
-[np.nan, np.nan, 7, 8, np.nan, np.nan, np.nan, 4, 9],
-[5, np.nan, 8, np.nan, np.nan, np.nan, 1, np.nan, 6],
-[np.nan, np.nan, 9, np.nan, np.nan, np.nan, 4, np.nan, np.nan],
-[np.nan, np.nan, np.nan, np.nan, 7, 2, np.nan, 1, 5],
-[7, np.nan, np.nan, 1, np.nan, 4, np.nan, np.nan, np.nan]])
+[
+[3, 4, 0, 2, 9, 8, 7, 0, 1],
+[9, 6, 0, 0, 5, 0, 0, 0, 0],
+[8, 7, 0, 6, 0, 1, 5, 9, 3],
+[1, 0, 0, 0, 2, 0, 0, 0, 0],
+[0, 0, 7, 8, 0, 0, 0, 4, 9],
+[5, 0, 8, 0, 0, 0, 1, 0, 6],
+[0, 0, 9, 0, 0, 0, 4, 0, 0],
+[0, 0, 0, 0, 7, 2, 0, 1, 5],
+[7, 0, 0, 1, 0, 4, 0, 0, 0]])
 
 
-values = np.isnan(ex1)
-row_sum = values.sum(axis=0)
-col_sum = values.sum(axis=1).reshape(9,1)
+def CalculateScore(matrix):
+    test = np.zeros((9,9))
 
-def sqSum(matrix):
-    row_sum = []
-    col_sum = []
     for i in range(0, 9, 3):
-        row_sum = np.append(row_sum, matrix[i:i+3,i:i+3].sum(axis=0))
-        col_sum = np.append(col_sum, matrix[i:i+3,i:i+3].sum(axis=1))
-    return row_sum + col_sum.reshape(9,1)
+        for j in range(0, 9,3 ):
+            test[i:i+3,j:j+3] = matrix[i:i+3,j:j+3].sum()
+    return test
 
 
+isFound = (ex1 == 0)
+row_sum = isFound.sum(axis=0)
+col_sum = isFound.sum(axis=1).reshape(9,1)
 
-result = row_sum + col_sum + sqSum(values)
-print(result.index(min(result)))
+result = (row_sum + col_sum + CalculateScore(isFound)) * isFound
+nextToDetermine = np.unique(result)[1]
+print(nextToDetermine)
+indexOfNext = np.where(result == nextToDetermine)
+i = indexOfNext[0][0]
+j = indexOfNext[1][0]
+
+def FindMissingNumber(i, j):
+    for num in range(1,9):
+        if num not in ex1[i,:]:
+            if num not in (ex1[:,j]): #ook nog checken wel kwadrant
+                print("found the number " + str(num) + " at position " + str(i) + " " + str(j))
+                ex1[i,j] = num
+
+    return ex1
+
+FindMissingNumber(i,j)
+
+## ik hou nog geen rekening met het feit dat je ook naar andere lijnen/ kolommen moet kijken naast diegene waar je in zit
